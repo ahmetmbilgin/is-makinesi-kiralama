516999a99eed8e9890e8112dc8d4a675afc2697f
ahmetmbilgin <ahmet.m.bilgin@hotmail.com>
2026-08-30 23:45:56 +0300

    Vercel Analytics + Speed Insights ekleniyor

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import Head from "next/head"
 import Layout from "@/components/Layout"
 import { poppins, openSans } from "@/lib/fonts"
 import { SITE_URL, SITE_NAME, SITE_TEL } from "@/lib/site"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   return (
@@ -45,6 +47,8 @@ export default function App({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Analytics />
+      <SpeedInsights />
     </div>
   )
 }
